31a62a2cbc55054d7aded82125cc84cd169f8ed3
JoaoJose <email@joaojose.com.br>
2013-11-25 20:46:02 -0200

    colocando composer

--- Console/Command/BuildShell.php
+++ Console/Command/BuildShell.php
@@ -4,6 +4,8 @@ class BuildShell extends AppShell {
         $this->out('Rodando os comandos:');
         $this->out('webdispatcher@' . (env('HTTP_HOST') ?: 'localhost') . ' $~/ cd '. APP);
         passthru('cd '. APP);
+        $this->out('webdispatcher@' . (env('HTTP_HOST') ?: 'localhost') . ' $'.APP.' ~/libs/composer.phar install');
+        passthru('~/libs/composer.phar install');
         $this->out('webdispatcher@' . (env('HTTP_HOST') ?: 'localhost') . ' $'.APP.' npm install');
         passthru('npm install');
         $this->out('webdispatcher@' . (env('HTTP_HOST') ?: 'localhost') . ' $'.APP.' bower install');
