724b0767ccce9ba077b4a35d76ce50e5bc68c473
JoaoJose <email@joaojose.com.br>
2013-11-25 21:03:17 -0200

    acertando

--- Controller/ConsoleController.php
+++ Controller/ConsoleController.php
@@ -19,7 +19,7 @@ class ConsoleController extends Controller
         ));
         $this->request->addDetector('internal-host', array(
             'env' => 'HTTP_HOST',
-            'options' => array('Internal.host')
+            'options' => Configure::read('Internal.ip')
         ));
 
         try {
